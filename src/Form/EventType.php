fbe187d900fabeb2bc502838b2a433bfb24756e5
MagicMaxim <kazargis@gmail.com>
2019-04-25 15:11:27 +0300

    Pushinu
    .

---
+++ src/Form/EventType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Event;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+
+class EventType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title')
+            ->add('category', ChoiceType::class, [
+                'choices'  => [
+                    'Sports' => "Sportas",
+                    'Science' => "Science",
+                    'Art' => "Art",
+                    'Entertainment' => "Entertainment",
+                ],
+            ])
+            ->add('description')
+            ->add('location')
+            ->add('price')
+            ->add('date')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Event::class,
+        ]);
+    }
+}
